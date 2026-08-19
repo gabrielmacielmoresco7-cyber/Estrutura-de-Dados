235e3664726c3112de73a894749c337d2553cd26
gabrielmacielmoresco7-cyber <gabrielmacielmoresco7@gmail.com>
2026-08-19 19:48:34 -0300

    Estrutura De dados Atividade

---
+++ Estrutura-de-dados-main/exercicio1/Program.cs
@@ -0,0 +1,37 @@
+﻿// crie um programa que seja capaz de ler 10 numeros inteiros e separalos em dois vetores, um para numeros pares e outro para numeros impares. Ao final, imprima os pares e depois impares
+
+ int[] pares = new int[10];
+ int[] impares = new int[10];
+
+int quantidadepar = 0;
+int quantidadeimpar = 0;
+
+ for (int i = 0; i < 10; i++)
+        {
+            Console.Write("Digite um número inteiro: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                pares[quantidadepar] = numero;
+                quantidadepar++;
+            }
+            else
+            {
+                impares[quantidadeimpar] = numero;
+                quantidadeimpar++;
+            }
+        };
+       Console.WriteLine("\nNúmeros pares:");
+        for (int i = 0; i < quantidadepar; i++)
+            {
+                 Console.Write(pares[i] + " ");
+}
+
+                Console.WriteLine("\n\nNúmeros ímpares:");
+        for (int i = 0; i < quantidadeimpar; i++)
+            {
+              Console.Write(impares[i] + " ");
+}
+
+Console.WriteLine("\n");
---
+++ Estrutura-de-dados-main/exercicio2/Program.cs
@@ -0,0 +1,27 @@
+﻿string palavra = "";
+
+Console.Write("Digite uma palavra: ");
+palavra = Console.ReadLine();
+
+string[] letras = new string[palavra.Length];
+
+for (int i = 0; i < palavra.Length; i++)
+{
+    letras[i] = palavra[i].ToString();
+}
+
+string invertida = "";
+
+for (int i = letras.Length - 1; i >= 0; i--)
+{
+    invertida += letras[i];
+}
+
+if (palavra == invertida)
+{
+    Console.WriteLine("A palavra é um palíndromo.");
+}
+else
+{
+    Console.WriteLine("A palavra não é um palíndromo.");
+}
